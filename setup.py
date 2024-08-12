a193e2fdab3a13286bceb8472582b835a773770f
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-08-13 01:52:52 +0400

    Moving to structured package

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hermes-cli",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "anthropic",
+        "boto3",
+        "google-generativeai",
+        "python-docx",
+        "PyPDF2",
+        "rich",
+        "openai",
+        "argcomplete"
+    ],
+    entry_points={
+        "console_scripts": [
+            "hermes=hermes.main:main",
+        ],
+    },
+)
