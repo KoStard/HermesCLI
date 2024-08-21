5ff7e3d80182c57eacdf9ba938807d20cb360c03
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-08-21 16:17:43 +0200

    Minor improvement

--- src/hermes/chat_application.py
+++ src/hermes/chat_application.py
@@ -26,7 +26,7 @@ class ChatApplication:
             return
 
         context = self.prompt_formatter.format_prompt(self.files, first_message, special_command)
-        response = self.process_message(context)
+        response = self.ui.display_response(self.model.send_message(context))
         
         if special_command:
             self.handle_special_command(special_command, response)
@@ -40,10 +40,6 @@ class ChatApplication:
                 return
 
             self.ui.display_response(self.model.send_message(user_input))
-
-    def process_message(self, message: str) -> str:
-        response_generator = self.model.send_message(message)
-        return self.ui.display_response(response_generator)
 
     def handle_special_command(self, special_command: Dict[str, str], content: str):
         if 'append' in special_command:
