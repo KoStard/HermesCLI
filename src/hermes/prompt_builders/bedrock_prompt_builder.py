6fd27cb5713b85c0cf92e0ec3972510037d9c9b8
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-09-04 12:11:03 +0200

    feat: add file processor and binary file check to BedrockPromptBuilder

--- src/hermes/prompt_builders/bedrock_prompt_builder.py
+++ src/hermes/prompt_builders/bedrock_prompt_builder.py
@@ -1,10 +1,14 @@
 from typing import Optional, List, Dict, Union, Any
+
+from hermes.file_processors.base import FileProcessor
 from .base import PromptBuilder
+from hermes.utils.file_utils import is_binary
 
 class BedrockPromptBuilder(PromptBuilder):
-    def __init__(self):
+    def __init__(self, file_processor: FileProcessor):
         self.contents: List[Dict[str, Union[str, Dict[str, Any]]]] = []
         self.system_message_added = False
+        self.file_processor = file_processor
 
     def add_text(self, text: str, name: Optional[str] = None):
         if not self.system_message_added:
@@ -16,7 +20,7 @@ class BedrockPromptBuilder(PromptBuilder):
         content = text if not name else f"{name}:\n{text}"
         self.contents.append({'text': content + '\n'})
 
-    def add_file(self, file_path: str, name: Optional[str] = None):
+    def add_file(self, file_path: str, name: str):
         file_name = name or file_path
         self.contents.append({
             'text': f"File: {file_name}\nContent:\n{{file_content:{file_path}}}\n"
