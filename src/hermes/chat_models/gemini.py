38822efb888ab74dec65053a853277c838190292
KoStard <rub099999720@gmail.com>
2024-09-30 18:40:47 +0200

    feat: add support for multiple Gemini model versions

--- src/hermes/chat_models/gemini.py
+++ src/hermes/chat_models/gemini.py
@@ -4,18 +4,26 @@ import google.generativeai as genai
 from google.generativeai.types import HarmCategory, HarmBlockThreshold
 from ..registry import register_model
 
-@register_model(name="gemini", file_processor="default", prompt_builder="xml", config_key='GEMINI')
+
+@register_model(name=[
+    "gemini"
+    "gemini/1.5-pro-exp-0801",
+    "gemini/1.5-pro-exp-0827",
+    "gemini/1.5-pro-002"
+], file_processor="default", prompt_builder="xml", config_key='GEMINI')
 class GeminiModel(ChatModel):
     def initialize(self):
         api_key = self.config.get("api_key")
         if not api_key:
             raise ValueError("API key is required for Gemini model")
+        model_identifier = self.config["model_identifier"]
+        model_name = self.get_model_id(model_identifier)
         genai.configure(api_key=api_key)
-        model_name = self.config.get("model", "gemini-1.5-pro-exp-0801")
         self.model = genai.GenerativeModel(model_name)
 
     def send_history(self, messages) -> Generator[str, None, None]:
-        history = [genai.types.ContentDict(role=msg['role'], parts=[msg['content']]) for msg in messages[:-1]]
+        history = [genai.types.ContentDict(
+            role=msg['role'], parts=[msg['content']]) for msg in messages[:-1]]
         chat = self.model.start_chat(history=history)
         response = chat.send_message(messages[-1]['content'], stream=True, safety_settings={
             HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
@@ -25,3 +33,13 @@ class GeminiModel(ChatModel):
         })
         for chunk in response:
             yield chunk.text
+
+    def get_model_id(self, model_identifier):
+        if model_identifier == 'gemini/1.5-pro-exp-0801':
+            return 'gemini-1.5-pro-exp-0801'
+        elif model_identifier == 'gemini/1.5-pro-exp-0827':
+            return 'gemini-1.5-pro-exp-0827'
+        elif model_identifier == 'gemini/1.5-pro-002':
+            return 'gemini-1.5-pro-002'
+        elif model_identifier == 'gemini':
+            return self.config.get("model", "gemini-1.5-pro-exp-0827")
