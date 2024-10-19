7a197fb17698130c1a5f22f5a30afdcdebb52064
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-10-19 07:01:14 +0200

    Fixing multiline input support

--- src/hermes/utils/commands_extractor.py
+++ src/hermes/utils/commands_extractor.py
@@ -2,7 +2,8 @@
 def extract_commands(source: str, commands_set: set[str], default_command: str = '/prompt') -> list[tuple[str, str]]:
     """
     Input example:
-    some input text
+    some input 
+    text
     /command1 arg1 arg2
     some other input text
     /command2 arg1 /command3 arg2
@@ -27,7 +28,7 @@ def extract_commands(source: str, commands_set: set[str], default_command: str =
         line = line.strip()
         words = line.split()
         if words and not (words[0].startswith('/') and words[0][1:] in commands_set):
-            if current_command:
+            if current_command and current_command != default_command:
                 result.append((current_command, ' '.join(current_args)))
                 current_args = []
             current_command = default_command
