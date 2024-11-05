f472b2f0c100b8d3144c4399fbffc2b25023cc05
KoStard <rub099999720@gmail.com>
2024-11-05 23:57:21 +0100

    fix: Gemini models not accepting empty system instruction

--- src/hermes/chat_models/gemini.py
+++ src/hermes/chat_models/gemini.py
@@ -28,7 +28,12 @@ class GeminiModel(ChatModel):
         if messages[0]['role'] == 'system':
             system_message = '\n'.join([m.get('text') for m in messages[0]['content'] if m.get('text')])
             messages = messages[1:]
-        self.model = genai.GenerativeModel(self.model_name, system_instruction=system_message)
+        
+        extra_args = {}
+        if system_message:
+            extra_args['system_instruction'] = system_message
+
+        self.model = genai.GenerativeModel(self.model_name, **extra_args)
 
         history = [ContentDict(
             role=self._get_message_role(msg['role']), parts=[msg['content']]) for msg in messages[:-1]]
