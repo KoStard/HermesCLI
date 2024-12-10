eb0a5fabb046b6757de48871931e5aed96e83957
Ruben Kostandyan <ruben.kostard@gmail.com>
2024-12-10 19:55:02 +0100

    fix: Terminal window size detection not working in piped scenarios

--- hermes/interface/helpers/terminal_window.py
+++ hermes/interface/helpers/terminal_window.py
@@ -11,7 +11,11 @@ def get_terminal_size() -> tuple[int, int]:
     Returns:
         tuple[int, int]: A tuple containing the number of rows and columns in the terminal.
     """
-    rows, columns = os.get_terminal_size()
+    try:
+        rows, columns = os.get_terminal_size()
+    except Exception as e:
+        print(f"Error getting terminal size: {e}")
+        rows, columns = 100, 100
     return rows, columns
 
 if __name__ == "__main__":
