26410396151f0838c49fc052f9d59d16ef3425ec
Ruben Kostandyan <ruben.kostard@gmail.com>
2025-01-10 13:37:47 +0400

    feat: Handle relative and from-home filepaths in messages

---
+++ hermes/utils/filepath.py
@@ -0,0 +1,28 @@
+import os
+
+def prepare_filepath(filepath: str) -> str:
+    """
+    Convert various filepath formats to a normalized absolute path.
+    
+    Args:
+        filepath: Input path that can be:
+            - filename.extension
+            - ./relative/path/filename.extension
+            - ../../relative/path/filename.extension
+            - /absolute/path/filename.extension
+            - ~/absolute/path/filename.extension
+            
+    Returns:
+        Normalized absolute path
+    """
+    # Expand user directory (~)
+    expanded_path = os.path.expanduser(filepath)
+    
+    # Convert to absolute path if relative
+    if not os.path.isabs(expanded_path):
+        expanded_path = os.path.abspath(expanded_path)
+        
+    # Normalize the path (resolve .. and . components)
+    normalized_path = os.path.normpath(expanded_path)
+    
+    return normalized_path
