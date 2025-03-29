3f5f9fdb0e727a4a2f605752fd55fc46638f2462
Ruben Kostandyan <ruben.kostard@gmail.com>
2025-03-29 17:59:36 +0100

    fix: render definition

--- hermes/interface/base.py
+++ hermes/interface/base.py
@@ -8,15 +8,16 @@ Basically interface is what the user sees and interacts with
 """
 
 from abc import ABC, abstractmethod
-from typing import Generator
+from typing import Generator, List
 from hermes.event import Event
 from hermes.history import History
+from hermes.message import Message
 
 
 class Interface(ABC):
     @abstractmethod
     def render(
-        self, events: Generator[Event, None, None]
+        self, history_snapshot: List[Message], events: Generator[Event, None, None]
     ) -> Generator[Event, None, None]:
         """
         Render events to the participant.
